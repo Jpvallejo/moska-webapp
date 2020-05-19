c1735b4e87f3d01a6fec40fbd5e0b0c1862ccc3d
Juan Pablo Vallejo <jvallejo@solstice.com>
2020-05-19 01:22:07 -0300

    prettier changes

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { LoginComponent } from './login.component';
-import { AuthServiceConfig, GoogleLoginProvider, FacebookLoginProvider, SocialLoginModule } from 'angularx-social-login';
+import {
+  AuthServiceConfig,
+  GoogleLoginProvider,
+  FacebookLoginProvider,
+  SocialLoginModule,
+} from 'angularx-social-login';
 import { ReactiveFormsModule } from '@angular/forms';
 import { environment } from 'src/environments/environment';
 import { AuthModule } from '../core/auth/auth.module';
@@ -10,37 +15,28 @@ import { LoginService } from './services/login.service';
 const config = new AuthServiceConfig([
   {
     id: GoogleLoginProvider.PROVIDER_ID,
-    provider: new GoogleLoginProvider(environment.OAUTH.GOOGLE_APP_ID)
+    provider: new GoogleLoginProvider(environment.OAUTH.GOOGLE_APP_ID),
   },
   {
     id: FacebookLoginProvider.PROVIDER_ID,
-    provider: new FacebookLoginProvider(environment.OAUTH.FACEBOOK_APP_ID)
-  }
+    provider: new FacebookLoginProvider(environment.OAUTH.FACEBOOK_APP_ID),
+  },
 ]);
 export function provideConfig() {
   return config;
 }
 
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
-  entryComponents: [
-  ],
-  imports: [
-    SharedModule,
-    AuthModule,
-    ReactiveFormsModule,
-    SocialLoginModule
-  ],
+  declarations: [LoginComponent],
+  entryComponents: [],
+  imports: [SharedModule, AuthModule, ReactiveFormsModule, SocialLoginModule],
   providers: [
     {
       provide: AuthServiceConfig,
-      useFactory: provideConfig
+      useFactory: provideConfig,
     },
-    LoginService
+    LoginService,
   ],
-  bootstrap: [LoginComponent]
+  bootstrap: [LoginComponent],
 })
-export class LoginModule {
-}
+export class LoginModule {}
